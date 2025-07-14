b783e3fba4638c4452c2f076a7f54ac2bec14d57
Alex <reak002@gmail.com>
2025-07-14 16:17:44 +0300

    Create ApiInterface.php

---
+++ src/ApiInterface.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace BitrixRestApi;
+
+use BitrixRestApi\Responser\Response\AbstractResponse;
+use Symfony\Component\HttpFoundation\Request;
+
+interface ApiInterface
+{
+    public function setRequest(Request $request);
+
+    public function getRequest(): Request;
+
+    public function setUser($user);
+
+    public function getUser();
+
+    public function response(AbstractResponse $response);
+}
